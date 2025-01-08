b5efc7c8fd08f2531fc8b8f952910893315ea6b6
phegde <pradnyahegde.null@gmail.com>
2025-01-08 18:33:15 -0500

    Added endpoints for testing with postman

--- index.js
+++ index.js
@@ -2,43 +2,6 @@ const express = require("express"),
   morgan = require("morgan");
 const app = express();
 
-let topMovies = [
-  {
-    Title: "The Shawshank Redemption",
-    Year: "1994",
-    Genre: "Drama",
-  },
-  {
-    Title: "The Godfather",
-    Year: "1972",
-    Genre: "Crime, Drama",
-  },
-  {
-    Title: "The Dark Knight",
-    Year: "2008",
-    Genre: "Action, Crime, Drama",
-  },
-  {
-    Title: "The Godfather: Part II",
-    Year: "1974",
-    Genre: "Crime, Drama",
-  },
-  {
-    Title: "12 Angry Men",
-    Year: "1957",
-    Genre: "Crime, Drama",
-  },
-  {
-    Title: "Schindler's List",
-    Year: "1993",
-    Genre: "Biography, Drama, History",
-  },
-  {
-    Title: "The Lord of the Rings: The Return of the King",
-    Year: "2003",
-    Genre: "Action, Adventure, Drama",
-  },
-];
 //middleware
 app.use(morgan("common"));
 
@@ -47,9 +10,39 @@ app.get("/", (req, res) => {
   res.send("Welcome to myFlix!");
 });
 
-app.get("/movies", (req, res) => {
-  res.json(topMovies);
+app.get("/movies", (req, res) => {  
+  res.send('Successful GET request returning data on all movies.');
 });
+
+app.get("/movies/:title", (req, res) => {
+  res.send('Successful GET request returning data for movie {title}.');
+});
+
+app.get("/genre/:genrename", (req, res) => {
+  res.send('Successful GET request returning info about the genre and a list of movies under that genre.');
+});
+app.get("/directors/:dirname", (req, res) => {  
+  res.send('Successful GET request returning a JSON object containing data(bio, birth year, death year,movies) about given directors name.');
+});
+app.post("/users/adduser", (req, res) => {  
+  res.send('Successfully added new user.');
+});
+app.post("/users/:username/favorites", (req, res) => {  
+  res.send('Successfully added movie to favories.');
+});
+app.put("/users/:username", (req, res) => {  
+  res.send('Successfully updated user info.');
+});
+app.post("/users/:username/favorites", (req, res) => { 
+  res.send('Successfully added movie to list to favorites list.');
+});
+app.delete("/users/:username/favorites/:movieid", (req, res) => {  
+  res.send('Successfully deleted {moviename} from favorites.');
+});
+app.delete("/users/:username", (req, res) => {  
+  res.send('Successfully deleted {username}.');
+});
+
 
 app.use(express.static("public"));
 

a775d45e039d185638642790b634dd2ca14e6311
phegde <pradnyahegde.null@gmail.com>
2025-01-19 20:29:14 -0500

    Fiexed error due to multiple declaration of userShcema variable.

--- models.js
+++ models.js
@@ -28,14 +28,6 @@ let userSchema = mongoose.Schema({
 let Movie = mongoose.model("Movie", movieSchema);
 let User = mongoose.model("User", userSchema);
 
-let userSchema = mongoose.Schema({
-  Username: { type: String, required: true },
-  Password: { type: String, required: true },
-  Email: { type: String, required: true },
-  Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
-});
-
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };

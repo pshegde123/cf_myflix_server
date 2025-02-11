beef5244e817e0866f05819105072c483c6b378d
Pradnya Hegde <pradnyahegde@gmail.com>
2025-02-10 21:43:14 -0500

    Updated allowedOrigins with netlify site url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true })); // Import body parser
 
 const cors = require("cors");
 /*app.use(cors());*/
-let allowedOrigins = ["http://localhost:8080", "http://localhost:1234","http://testsite.com"];
+let allowedOrigins = ["http://localhost:8080", "http://localhost:1234","http://testsite.com","https://react-myflix.netlify.app/signup"];
 
 app.use(
   cors({
